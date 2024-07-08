94eb43af040b5f502fd7c5d02957dd674505d963
FerMendoza123 <mendozafernando2023@gmail.com>
2024-07-08 14:00:06 -0500

    navigation bar and product components added

--- src/app/purchase/purchase.module.ts
+++ src/app/purchase/purchase.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { CartComponent } from './cart/cart.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ProductComponent } from './products-list/product/product.component';
 
 
 
 @NgModule({
   declarations: [
     ProductsListComponent,
-    CartComponent
+    CartComponent,
+    NavigationBarComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule
