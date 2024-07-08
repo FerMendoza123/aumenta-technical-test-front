7dab87c283aa467a0fa3ef897f5ef43d441c098f
FerMendoza123 <mendozafernando2023@gmail.com>
2024-07-08 14:51:33 -0500

    components relocated with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsListComponent } from './purchase/products-list/products-list.component';
+import { ProductComponent } from './purchase/products-list/product/product.component';
+import { CartComponent } from './purchase/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"purchase", component: ProductsListComponent},
+  {path:"cart", component: CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './purchase/navigation-bar/navigation-bar.component';
+import { ProductsListComponent } from './purchase/products-list/products-list.component';
+import { CartComponent } from './purchase/cart/cart.component';
+import { ProductComponent } from './purchase/products-list/product/product.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    CartComponent,
+    NavigationBarComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
